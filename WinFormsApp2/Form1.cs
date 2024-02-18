ad6f8139335d899f3dabab1104a64d2a720b12a8
314362134 <314362134@qq.com>
2024-02-18 22:07:16 +0800

    AFASF

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -5,6 +5,34 @@ namespace WinFormsApp2
         public Form1()
         {
             InitializeComponent();
+
+
+
+        }
+
+
+        private void test()
+        { 
+        
+        
+//<<<<<<< HEAD
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+//=======
+//>>>>>>> 4be99b894c60e7ee0da0c32117b7f1e2f0d3ddf3
         }
     }
 }
